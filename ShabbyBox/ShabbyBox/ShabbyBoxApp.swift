7b3c04dc4a381ce24cbbbfba53f41dc2f494cf87
seungyooooong <chadange@naver.com>
2024-01-19 17:06:35 +0900

    Create ShabbyBox

---
+++ ShabbyBox/ShabbyBox/ShabbyBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShabbyBoxApp.swift
+//  ShabbyBox
+//
+//  Created by 최승용 on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ShabbyBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
