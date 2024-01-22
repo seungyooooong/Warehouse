6fb6f9a0bbb2b6efe72e919fd4149ddc49724439
seungyooooong <chadange@naver.com>
2024-01-22 18:50:11 +0900

    Add Components
    1. Add UserList component

---
+++ ShabbyBox/ShabbyBox/View/Components/UserList.swift
@@ -0,0 +1,33 @@
+//
+//  UserList.swift
+//  ShabbyBox
+//
+//  Created by 최승용 on 1/22/24.
+//
+
+import SwiftUI
+
+struct UserList: View {
+    @ObservedObject var userViewModel: UserViewModel
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            ForEach(userViewModel.userList) { user in
+                Text(user.name)
+                    .font(.headline)
+                    .foregroundStyle(Color("oppositeColor"))
+            }
+        }
+        .hCenter()
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 15.0)
+                .fill(Color("mainColor"))
+                .padding(.horizontal)
+        )
+    }
+}
+
+#Preview {
+    UserList(userViewModel: UserViewModel())
+}
