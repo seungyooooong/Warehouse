a703a9bccfeb1bc265eed7b6d6e7f05438b548bf
seungyooooong <chadange@naver.com>
2024-01-29 17:22:45 +0900

    Add AppInfo

--- ShabbyBox/ShabbyBox/View/AppInfoView.swift
+++ ShabbyBox/ShabbyBox/View/AppInfoView.swift
@@ -10,20 +10,12 @@ import SwiftUI
 struct AppInfoView: View {
     var body: some View {
         ViewThatFits(in: .vertical) {
+            AppInfo()
+                .padding()
             ScrollView(.vertical, showsIndicators: false) {
-                VStack {
-                    GroupBox {
-                        AppInfoContent(name: "Developer", content: "seungyooooong")
-                        AppInfoContent(name: "Compatibility", content: "iOS16.0")
-                        AppInfoContent(name: "Version", content: "1.0.0")
-                        AppInfoContent(name: "Notion", linkLabel: "Go to Warehouse TIL", linkDestination: "https://www.notion.so/seungyooooong/Warehouse-5f82fe5b62814bb7b8482377b70ec4c3")
-                        AppInfoContent(name: "Github", linkLabel: "Go to Repository", linkDestination: "github.com/seungyooooong/Warehouse")
-                    } label: {
-                        AppInfoLabel(labelText: "Application", labelImage: "apps.iphone")
-                    }
-                    .padding(.vertical)
-                }
+                AppInfo()
             }
+            .padding()
         }
     }
 }
---
+++ ShabbyBox/ShabbyBox/View/Components/AppInfo.swift
@@ -0,0 +1,30 @@
+//
+//  AppInfo.swift
+//  ShabbyBox
+//
+//  Created by 최승용 on 1/29/24.
+//
+
+import SwiftUI
+
+struct AppInfo: View {
+    var body: some View {
+        VStack {
+            GroupBox {
+                AppInfoContent(name: "Developer", content: "seungyooooong")
+                AppInfoContent(name: "Compatibility", content: "iOS16.0")
+                AppInfoContent(name: "Version", content: "1.0.0")
+                AppInfoContent(name: "Notion", linkLabel: "Go to Warehouse TIL", linkDestination: "https://www.notion.so/seungyooooong/Warehouse-5f82fe5b62814bb7b8482377b70ec4c3")
+                AppInfoContent(name: "Github", linkLabel: "Go to Repository", linkDestination: "github.com/seungyooooong/Warehouse")
+            } label: {
+                AppInfoLabel(labelText: "Application", labelImage: "apps.iphone")
+            }
+            .padding(.vertical)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AppInfo()
+}
