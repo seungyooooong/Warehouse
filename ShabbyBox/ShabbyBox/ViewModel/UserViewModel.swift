d9b07ccba3544a7be3320e88096fe2cbbd96609b
seungyooooong <chadange@naver.com>
2024-02-05 15:51:36 +0900

    Modify UserViewModel

--- ShabbyBox/ShabbyBox/ViewModel/UserViewModel.swift
+++ ShabbyBox/ShabbyBox/ViewModel/UserViewModel.swift
@@ -10,36 +10,37 @@ import Foundation
 class UserViewModel: ObservableObject {
     @Published var userList: [UserModel] = Bundle.main.decode("User.json")
     
+    let encoder = JSONEncoder()
+    let decoder = JSONDecoder()
+    
     init() {
-//        initData()
-//        loadData()
+        
     }
     
-//    func initData() {
-//        let userModel = UserModel(name: "testUser1")
-//        userList.append(userModel)
-//    }
-    
     func addUser() {
-        let userData = UserModel(id: userList.count, name: "GoSil\(userList.count)", isLike: false)
+        let user = UserModel(id: userList.count, name: "GoSil\(userList.count)", isLike: false)
+        userList.append(user)
         
-        let encoder = JSONEncoder()
-        let decoder = JSONDecoder()
-        
-        do {
-            let jsonData = try encoder.encode(userData)
-            let jsonString = String(data: jsonData, encoding: .utf8)
-            guard let jsonStr = jsonString else { fatalError() }
-            guard let jsonData = jsonStr.data(using: .utf8) else { fatalError() }
-            let user = try decoder.decode(UserModel.self, from: jsonData)
-            userList.append(user)
-        } catch {
-            print("\(error) in decoding")
-        }
+//        let userData = UserModel(id: userList.count, name: "GoSil\(userList.count)", isLike: false)
+//        do {
+//            let jsonData = try encoder.encode(userData)
+//            let jsonString = String(data: jsonData, encoding: .utf8)
+//            guard let jsonStr = jsonString else { fatalError() }
+//            guard let jsonData = jsonStr.data(using: .utf8) else { fatalError() }
+//            let user = try decoder.decode(UserModel.self, from: jsonData)
+//            userList.append(user)
+//        } catch {
+//            print(error)
+//        }
     }
     
     func toggleIsLike(user: UserModel) {
-        // user.id를 통해 json파일에서 어떤 user인지 찾아내고,
-        // 해당 user의 isLike를 !user.isLike로 수정한다.
+        for i in 0 ..< userList.count {
+            if (userList[i].id == user.id) {
+                let newUser = UserModel(id: userList[i].id, name: userList[i].name, isLike: !user.isLike)
+                userList.remove(at: i)
+                userList.insert(newUser, at: i)
+            }
+        }
     }
 }
