7ffb96d9c0b159ea00faedb126935781c081b305
seungyooooong <chadange@naver.com>
2024-02-11 15:49:00 +0900

    Add annotation

--- ShabbyBox/ShabbyBox/ViewModel/UserViewModel.swift
+++ ShabbyBox/ShabbyBox/ViewModel/UserViewModel.swift
@@ -35,6 +35,7 @@ class UserViewModel: ObservableObject {
     }
     
     func addUser(userName: String) {
+        // id를 유니크하게 보장하는 로직 추가 전에 임시로 마지막 user의 id보다 1높게 설정, 추후 수정
         let user = UserModel(id: userList[userList.count - 1].id + 1, name: userName, isLike: false)
         userList.append(user)
         
