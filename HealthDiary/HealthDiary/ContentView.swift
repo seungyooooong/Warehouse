f84a7ad571fd6601b72834ac2890f1b9ee6a178b
seungyooooong <chadange@naver.com>
2024-03-05 22:14:15 +0900

    Add new Project

---
+++ HealthDiary/HealthDiary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthDiary
+//
+//  Created by 최승용 on 3/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthDiary/HealthDiary/HealthDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDiaryApp.swift
+//  HealthDiary
+//
+//  Created by 최승용 on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
