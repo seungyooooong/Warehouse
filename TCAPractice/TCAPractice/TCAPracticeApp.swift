c952def1986770feeb94a9b8d7fef8773637a080
chadange <chadange@naver.com>
2024-07-18 13:31:28 +0900

    Add TCAPractice & import packages

---
+++ TCAPractice/TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by 최승용 on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
